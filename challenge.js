712debdf2ce1cbe25e0a5c8df523468af26617ad
Jennifer Manriquez <jennymanb@gmail.com>
2021-10-29 14:34:53 -0500

    Can pass conditions as parameters to foobar function

--- challenge.js
+++ challenge.js
@@ -1,6 +1,6 @@
 //Mandar un diff por cada una de estas
 //✔️ agregar otro check: para multiplo de 10 imprimir bar, crear un tag con eso (tal vez necesite commit intermedios)
-//poder pasar checks como parametros de la funcion foo bar. 
+// ✔️ poder pasar checks como parametros de la funcion foo bar. 
 //configurar si la condicion es exclusiva o no. Que tenga que imprimir var con una o con todas
 
 function isPrime(num) {
@@ -16,23 +16,45 @@ function isPrime(num) {
   return true
 }
 
-function multipleTen(num) {
+function isMultipleOfTen(num) {
   if (num % 10 == 0) {
     return true
   }
+  return false
 }
 
-function foobar(n){
+function isBetweenNineAndEleven(num) {
+  if (num <= 11 && num >= 9) {
+    return true
+  }
+  return false
+}
+
+
+function foobar(n, ...conditions){
   for (let i=1; i<=n; i++) {
-    const numIsPrime = isPrime(i)
-    const numIsMultipleTen = multipleTen(i)
-    if (numIsPrime || numIsMultipleTen){
-      console.log(i, 'bar');
+    let toPrint = "";
+    let anyConditionPassed = false;
+
+    conditions.forEach((condition, index)=> {
+      const conditionCheck = condition(i);
+      toPrint = toPrint + ` cond${index}: ${conditionCheck},`
+      if (conditionCheck) {
+        anyConditionPassed = true
+      }
+    });
+
+    if (anyConditionPassed){
+      console.log(i, 'bar', toPrint);
     } else {
-      console.log(i, 'foo');
+      console.log(i, 'foo', toPrint);
     }
   }
 }
 
 
-foobar(30);
+//First argument of foobar is the number of prints
+//Rest of arguments are conditions to check, if any is true return var. 
+foobar(20, isPrime, isMultipleOfTen);
+console.log('-------------------------------------------');
+foobar(15, x => x <= 5, isBetweenNineAndEleven);
